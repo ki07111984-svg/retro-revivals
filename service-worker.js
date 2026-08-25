1ddeca1f35a07e3ce59c37630c3fa15433c3a1fa
ki07111984-svg <ki07111984@gmail.com>
2026-08-25 23:13:56 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,40 @@
-const CACHE='retro-revivals-v1';
-const ASSETS=[
- './','./index.html','./styles.css','./script.js','./manifest.webmanifest',
- './logo.jpg','./gaming.svg','./collectibles.svg','./toys.svg','./cards.svg',
- './favicon.svg','./icon-192.svg','./icon-512.svg'
+const CACHE_NAME = "retro-revivals-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.svg",
+  "./icon-512.svg",
+  "./favicon.svg",
+  "./logo.jpg"
 ];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
